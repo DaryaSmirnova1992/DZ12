c508c9b20b555e8c34874539861b270808f2dccd
Darya <darecc@yandex.ru>
2022-09-19 19:50:11 +0400

    add Task 64

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите значение М: ");
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.WriteLine("Введите значение М: ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение N: ");
 int N = Convert.ToInt32(Console.ReadLine());
