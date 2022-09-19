a90fe02abd7ee67d9ae8ec99c1335fb9d90e2712
Darya <darecc@yandex.ru>
2022-09-19 19:49:27 +0400

    add Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите значение М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"M={M}, N={N}");
+if(N>M)
+{
+    for (int i = M; i < N; i++)
+    {
+        Console.Write($"{i + ", "}");
+    }
+}
+else
+for (int i = N; i < M; i++)
+{
+    Console.Write($"{i + ", "}");
+}
